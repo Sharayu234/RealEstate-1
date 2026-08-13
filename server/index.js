a8b7348e5a73fdd1c1bc030adac23f4fc17f10bb
Sharayu Meshram <sharayumeshram3@gmail.com>
2026-08-13 17:57:47 +0530

    Fix CORS for deployed frontend

--- server/index.js
+++ server/index.js
@@ -12,7 +12,11 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors())
+
+app.use(cors({
+    origin: "https://real-estate-1-client-f8347klq6-sharayu234s-projects.vercel.app",
+    credentials: true
+}))
 
 app.get('/', (req, res) => {
     res.send('Real Estate API is running')
